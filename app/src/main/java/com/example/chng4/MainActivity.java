12f9353fbcf24031f67ba54d749e610e7dd6f110
Qkhanhs <2254810158@vaa.edu.vn>
2024-05-03 22:19:25 +0700

    layout trang dau

--- app/src/main/java/com/example/chng4/MainActivity.java
+++ app/src/main/java/com/example/chng4/MainActivity.java
@@ -17,23 +17,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        btnChuyenMH= findViewById(R.id.btnDoimanhinh);
+
     }
 
-    public void sukienChuyenmanHinh(View view) {
-        //Tao button trong trang moi
-        Button btnMoi= new Button(MainActivity.this){
-            //onclick quay ve giao dien cu
-            @Override
-            public boolean performLongClick() {
-                setContentView(R.layout.activity_main);
-                return super.performLongClick();
-            }
-        };
-        btnMoi.setText("Quay ve");
-        btnMoi.setHeight(400);
-        btnMoi.setWidth(200);
-        //hien thi button moi trong man hinh
-        setContentView(btnMoi);
-    }
+
 }
