3e05a17c7c722125d4a8855a9e62ac911b4e1b59
gsggsfsasdsdasd <168271459+gsggsfsasdsdasd@users.noreply.github.com>
2024-05-15 13:45:29 +0700

    Update Webkit.java

--- app/src/main/java/com/example/chng4/Webkit.java
+++ app/src/main/java/com/example/chng4/Webkit.java
@@ -1,7 +1,10 @@
 package com.example.chng4;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,20 +14,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Webkit extends AppCompatActivity {
 
-    Button btnBack;
+    private WebView webView;
 
+    @SuppressLint({"SetJavaScriptEnabled", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView( R.layout.layout_webkit);
-        btnBack = findViewById(R.id.BackWebkit);
-     btnBack.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View v) {
-             finish();
-         }
-     });
+        setContentView(R.layout.layout_webkit);
+
+        webView = findViewById(R.id.Webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://developer.android.com/jetpack/androidx/releases/webkit?hl=vi");
     }
-
-
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
